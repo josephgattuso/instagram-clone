2b2180beadeb263a8b1a4d603d25c716b8084130
josephgattuso <joseph.gattuso@gmail.com>
2020-07-30 03:43:05 +0100

    config: add react-ga :wrench:

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from "react";
 import Title from "./components/Title";
 import UploadForm from "./components/UploadForm";
 import ImageGrid from "./components/ImageGrid";
+import ReactGA from "react-ga";
+
+function initializeReactGA() {
+  ReactGA.initialize("G-MLZF68H9M9");
+  ReactGA.pageview("/firegram");
+}
 
 function App() {
   return (
     <div className="App">
+      <initializeReactGA />
       <Title />
       <UploadForm />
       <ImageGrid />
