29b00ed6999e4e5f7b046eacb99a9d9508c092b3
josephgattuso <joseph.gattuso@gmail.com>
2020-07-30 02:50:44 +0100

    feat: add progress bar :sparkles:

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,32 +1,34 @@
 import React, { useState } from "react";
+import ProgressBar from "./ProgressBar";
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
 
-  const types = ["image/png", "image/jpg"];
+  const types = ["image/png", "image/jpeg"];
 
-  const changeHandler = (e) => {
+  const handleChange = (e) => {
     let selected = e.target.files[0];
 
     if (selected && types.includes(selected.type)) {
       setFile(selected);
       setError("");
     } else {
-      setFile("");
-      setError("Please select an image file (png or jpg");
+      setFile(null);
+      setError("Please select an image file (png or jpg)");
     }
   };
 
   return (
     <form>
       <label>
-        <input type="file" onChange={changeHandler} />
+        <input type="file" onChange={handleChange} />
         <span>+</span>
       </label>
       <div className="output">
         {error && <div className="error">{error}</div>}
         {file && <div>{file.name}</div>}
+        {file && <ProgressBar file={file} setFile={setFile} />}
       </div>
     </form>
   );
