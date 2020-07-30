389581941ddefade692f439ecc6948d5635f81ed
josephgattuso <joseph.gattuso@gmail.com>
2020-07-30 11:59:42 +0100

    feat: add animation :sparkles:

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useFirestore from "../hooks/useFirestore";
+import { motion } from "framer-motion";
 
 const ImageGrid = ({ setSelectedImg }) => {
   const { docs } = useFirestore("images");
@@ -8,13 +9,22 @@ const ImageGrid = ({ setSelectedImg }) => {
     <div className="img-grid">
       {docs &&
         docs.map((doc) => (
-          <div
+          <motion.div
             className="img-wrap"
             key={doc.id}
+            layout
+            whileHover={{ opacity: 1 }}
+            s
             onClick={() => setSelectedImg(doc.url)}
           >
-            <img src={doc.url} alt="uploaded pic" />
-          </div>
+            <motion.img
+              src={doc.url}
+              alt="uploaded pic"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+            />
+          </motion.div>
         ))}
     </div>
   );
