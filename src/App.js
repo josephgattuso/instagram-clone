1b65e6dc78edc48275c0299d28eba203fa3e77d4
josephgattuso <joseph.gattuso@gmail.com>
2020-08-21 20:22:50 +0100

    add signup modal

--- src/App.js
+++ src/App.js
@@ -1,61 +1,132 @@
 import React, { useEffect, useState } from "react";
+import { Button, Modal, Input } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import styled from "styled-components";
+
+import GlobalStyles from "./components/GlobalStyles";
 import Header from "./components/Header";
 import UploadForm from "./components/UploadForm";
 import ImageGrid from "./components/ImageGrid";
-import Modal from "./components/Modal";
+import OldModal from "./components/OldModal";
 import Post from "./components/Post";
-import GlobalStyles from "./components/GlobalStyles";
 import Footer from "./components/Footer";
 
-import { db } from "./firebase/config";
+import { db, auth } from "./firebase/config";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    position: "absolute",
+    width: 400,
+  },
+}));
 
 function App() {
-  // eslint-disable-next-line
-  const [posts, setPosts] = useState([
-    // {
-    //   username: "anonymous",
-    //   imageUrl: "https://placeimg.com/640/480/tech",
-    //   caption: "awesome tech",
-    // },
-    // {
-    //   username: "anonymous",
-    //   imageUrl: "https://placeimg.com/640/480/animals",
-    //   caption: "such lovely animals",
-    // },
-    // {
-    //   username: "anonymous",
-    //   imageUrl: "https://placeimg.com/640/480/nature",
-    //   caption: "getting back to nature",
-    // },
-  ]);
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  const [username, setUsername] = useState(false);
+  const [email, setEmail] = useState(false);
+  const [password, setPassword] = useState(false);
 
   const [selectedImg, setSelectedImg] = useState(null);
 
-  // useEffect -> runs code based ona condition
   useEffect(() => {
-    // this is where the code runs
     db.collection("posts").onSnapshot(snapshot => {
-      // every time a new post is added, this code runs
-      setPosts(snapshot.docs.map(doc => doc.data()));
+      setPosts(snapshot.docs.map(doc => ({ id: doc.id, post: doc.data() })));
     });
   }, []);
+
+  const signUp = event => {
+    event.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .catch(error => alert(error.message));
+  };
+
+  const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+  `;
 
   return (
     <div className="app">
       <GlobalStyles />
+
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <Form>
+            <center>
+              <img
+                alt=""
+                src="https://josephgattuso.github.io/firegram/logo192.png"
+                width="48px"
+              />
+            </center>
+            <Input
+              onChange={e => setUsername(e.target.value)}
+              placeholder="username"
+              type="text"
+              value={username}
+            />
+            <Input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>
+              Sign Up
+            </Button>
+          </Form>
+        </div>
+      </Modal>
+
       <Header />
+
+      <Button onClick={() => setOpen(true)}>Sign-up</Button>
+
       <UploadForm />
-      {posts.map(post => (
+
+      {posts.map(({ id, post }) => (
         <Post
+          key={id}
           username={post.username}
           imageUrl={post.imageUrl}
           caption={post.caption}
         />
       ))}
+
       <ImageGrid setSelectedImg={setSelectedImg} />
       {selectedImg && (
-        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+        <OldModal selectedImg={selectedImg} setSelectedImg={setSelectedImg}>
+          <div style={modalStyle} className={classes.paper} />
+        </OldModal>
       )}
+
       <Footer />
     </div>
   );
